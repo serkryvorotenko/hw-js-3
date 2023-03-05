e1c27da35d4dc8397dcb49e8f1f44ce551ecd3ff
Serhii Kryvorotenko <serhii@MacBook-Pro-Serhii.local>
2023-03-05 21:13:42 +0100

    hw-4

--- js/script.js
+++ js/script.js
@@ -2,34 +2,48 @@ const operation = prompt('What operation should be performed? + , - , * , / , si
 let number1;
 let number2;
 
-if (operation === 'cos' || operation === 'sin') {
-    number1 = +prompt('what is the number?');
-} else {
-    number1 = +prompt('what is the first number?');
-    number2 = +prompt('what is the second number?');
+switch (operation) {
+    case `cos`:
+    case `sin`:
+        number1 = +prompt(`what is the number?`);
+        break;
+    default:
+        number1 = +prompt('what is the first number?');
+        number2 = +prompt('what is the second number?');
+        break;
 }
 
-if (operation === '+') {
-    const result = number1 + number2;
-    alert(`${number1} + ${number2} = ${result}`);
-} else if (operation === '-') {
-    const result = number1 - number2;
-    alert(`${number1} - ${number2} = ${result}`);
-} else if (operation === '*') {
-    const result = number1 * number2;
-    alert(`${number1} * ${number2} = ${result}`);
-} else if (operation === '/') {
-    const result = number1 / number2;
-    alert(`${number1} / ${number2} = ${result}`);
-} else if (operation === 'pow') {
-    const result = number1 ** number2;
-    alert(`${number1}ˆ${number2} = ${result}`);
-} else if (operation === 'sin') {
-    const result = Math.sin(number1);
-    alert(`sin ${number1} = ${result}`);
-} else if (operation === 'cos') {
-    const result = Math.cos(number1);
-    alert(`cos ${number1} = ${result}`);
-} else {
-    alert('such an action is impossible');
+let result;
+switch (operation) {
+    case '+':
+        result = number1 + number2;
+        alert(`${number1} + ${number2} = ${result}`);
+        break;
+    case '-':
+        result = number1 - number2;
+        alert(`${number1} - ${number2} = ${result}`);
+        break;
+    case `*`:
+        result = number1 * number2;
+        alert(`${number1} * ${number2} = ${result}`);
+        break;
+    case `/`:
+        result = number1 / number2;
+        alert(`${number1} / ${number1} = ${result}`);
+        break;
+    case `pow`:
+        result = number1 ** number2;
+        alert(`${number1} ˆ ${number2} = ${result}`);
+        break;
+    case `sin`:
+        result = Math.sin(number1);
+        alert(`sin ${number1} = ${result}`);
+        break;
+    case `cos`:
+        result = Math.cos(number1);
+        alert(`cos ${number1} = ${result}`);
+        break;
+    default:
+        alert('such an action is impossible');
+        break;
 }
